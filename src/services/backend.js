5c4c8d9d028b31059a67bfca0c4b2d402fa30460
raghad <raghad.hav@gmail.com>
2020-12-18 00:53:39 +0300

    Fixing the backend URL

--- src/services/backend.js
+++ src/services/backend.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-const baseUrl = 'http://localhost:3002/api/persons';
-//'/api/persons';
+const baseUrl = '/api/persons'; //'http://localhost:3002/api/persons';
 const getAll = () => {
   const request = axios.get(baseUrl);
   return request.then((response) => response.data);
